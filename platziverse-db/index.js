0fef016b47612843858dff28b5b3f5b573f1a082
palafoxernesto <palafox.ernesto@gmail.com>
2017-09-08 12:10:21 -0500

    Mocks y stubs con sinon

---
+++ platziverse-db/index.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+const defaults = require('defaults')
+
+module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: 'sqlite',
+    pools: {
+      max: 10,
+      min: 0,
+      idle: 10000
+    },
+    query: {
+      raw: true
+    }
+  })
+
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequelize.authenticate()
+
+  if (config.setup) {
+    await sequelize.sync({ force: true })
+  }
+
+  const Agent = {}
+  const Metric = {}
+
+  return {
+    Agent,
+    Metric
+  }
+}
